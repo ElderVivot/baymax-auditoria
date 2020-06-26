93dc727ec024633de66e1d81938939555fa0c290
= <=>
2020-06-26 19:58:07 -0300

    Adaptado a leitura de NF-e pra retornar um array, de acordo o modelo de NFS-e de Goiania retorna

--- backend/fiscal/src/services/read_files/NFe.py
+++ backend/fiscal/src/services/read_files/NFe.py
@@ -13,22 +13,39 @@ import tools.funcoesUteis as funcoesUteis
 class NFe(object):
     def __init__(self, dataXml):
         self._dataXml = dataXml
+        self._nfs = []
 
     def readNFe(self):
+        objNF = {}
+
         keyNF = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', '@Id'])
         keyNF = keyNF[3:]
+        objNF['keyNF'] = keyNF
         
         numberNF = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'ide', 'nNF'])
+        objNF['numberNF'] = numberNF
         
         issueDateNF = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'ide', 'dhEmi'])
         issueDateNF2 = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'ide', 'dEmi'])
         issueDateNF = issueDateNF2 if ( issueDateNF == "" or issueDateNF is None ) else issueDateNF
         issueDateNF = funcoesUteis.transformDateFieldToString(funcoesUteis.retornaCampoComoData(issueDateNF, 2))
+        objNF['issueDateNF'] = issueDateNF
 
         modelNF = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'ide', 'mod'])
+        objNF['modelNF'] = modelNF
+
         serieNF = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'ide', 'serie'])
+        objNF['serieNF'] = serieNF
+
         valueNF = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'total', 'ICMSTot', 'vNF'])
+        objNF['valueNF'] = valueNF
+
         valueICMS = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'total', 'ICMSTot', 'vICMS'])
+        objNF['valueICMS'] = valueICMS
+
+        refNFe = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'ide', 'NFref', 'refNFe'])
+        objNF['refNFe'] = refNFe
+
         nameIssuer = funcoesUteis.justLettersNumbersDots(funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'emit', 'xNome']))
         nameReceiver = funcoesUteis.justLettersNumbersDots(funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'dest', 'xNome']))
 
@@ -53,21 +70,25 @@ class NFe(object):
             cnpjReceiverCorrect = cnpjReceiver
             nameReceiverCorrect = nameReceiver
 
-        produtos = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'det'])
+        objNF['cnpjIssuerCorrect'] = cnpjIssuerCorrect
+        objNF['nameIssuerCorrect'] = nameIssuerCorrect
+        objNF['cnpjReceiverCorrect'] = cnpjReceiverCorrect
+        objNF['nameReceiverCorrect'] = nameReceiverCorrect
 
-        return {
-            "numberNF": numberNF,
-            "serieNF": serieNF,
-            "modelNF": modelNF,
-            "typeNF": typeNF,
-            "issueDateNF": issueDateNF,
-            "valueNF": valueNF,
-            "valueICMS": valueICMS,
-            "nameIssuer": nameIssuerCorrect,
-            "cnpjIssuer": cnpjIssuerCorrect,
-            "nameReceiver": nameReceiverCorrect,
-            "cnpjReceiver": cnpjReceiverCorrect,
-            "keyNF": keyNF,
-            "statusNF": 0, # ativa
-            "produtos": produtos
-        }
+        # produtos = funcoesUteis.returnDataFieldInDict(self._dataXml, ['nfeProc', 'NFe', 'infNFe', 'det'])
+        # objNF['produtos'] = produtos
+
+        self._nfs.append(objNF)
+
+        return self._nfs
+
+
+if __name__ == "__main__":
+    dataXml = readXml("C:/notas_fiscais/modelo_55/52191006314327000203550010057739601112482590.xml")
+
+    # with open("C:/_temp/notas_gyn_teste/04605182000185.xml") as file:
+    #     data = xmldict.parse(file.read())
+    #     print(xmldict.unparse(data))
+
+    nf = NFe(dataXml)
+    print(nf.readNFe())
